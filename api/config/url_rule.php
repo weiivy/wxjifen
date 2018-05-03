da129c9509c92cd1173ec1418793124c3aba18f6
zhangweiwei <118liuyixueer@sina.cn>
2018-05-03 09:03:19 +0800

    edit

--- api/config/url_rule.php
+++ api/config/url_rule.php
@@ -4,6 +4,22 @@
  * @copyright 2017-07-03
  * @author Serena Liu<serena.liu@ipptravel.com>
  */
+
+var_dump([
+    'POST  login'                                                => 'site/wxlogin',
+    'POST sendSms'                                               => 'site/sendSms',
+    'POST check-member'                                          => 'site/check-member',
+    'POST get-mobile'                                            => 'site/get-mobile',
+    'POST register'                                              => 'member/register',
+    'POST get-capital-detail'                                    => 'member/get-capital-detail',
+    'POST get-orders'                                            => 'member/get-orders',
+    'POST bank-config'                                           => 'site/bank-config',
+    'POST member'                                                => 'member/member',
+    'POST friends'                                               => 'member/friends',
+    'POST add-order'                                             => 'record/add-order',
+    'GET index'                                                  => 'site/index'
+]);die;
+
 return [
     'POST  login'                                                => 'site/wxlogin',
     'POST sendSms'                                               => 'site/sendSms',
