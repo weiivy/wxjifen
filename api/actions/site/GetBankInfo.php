cfed544c8050caa1422dd9b5af03b682ee4cf529
zhangweiwei <118liuyixueer@sina.cn>
2018-06-24 15:14:01 +0800

    edit

--- api/actions/site/GetBankInfo.php
+++ api/actions/site/GetBankInfo.php
@@ -46,7 +46,7 @@ class GetBankInfo extends BaseAction
                 'id'   => $bank['id'],
                 'bank' => $bank['bank'],
                 'bankName' => $bank['bank_name'],
-                'note' => ''
+                'note' => $bank['note']
             ];
             foreach ($bank['bankConfig'] as $bankConfig){
                 $data[$bank['id']]['list'][] = [
