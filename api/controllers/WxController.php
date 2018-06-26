a5c70f41ba5a1991335131824875798dbc874a16
zhangweiwei <118liuyixueer@sina.cn>
2018-06-26 23:18:33 +0800

    edit

--- api/controllers/WxController.php
+++ api/controllers/WxController.php
@@ -55,40 +55,37 @@ class WxController extends Controller
     {
         $flashKey = 'oAuthAuthState';
         $session = Yii::$app->session;
-        var_dump($session->get($flashKey));die;
+//        var_dump($session->get($flashKey));die;
 
         //从微信oAuth页面跳转回来
-        if ($session->get($flashKey)) {
-            $state = $session->get($flashKey);
-            $session->remove($flashKey);
-            if (!(isset($_POST['code']) && isset($_POST['state']) && $_POST['state'] === $state)) {
-                Yii::error('网页授权获取用户基本信息错误，请检查appid等相关信息');
-                return [
-                    'status' => 0,
-                    'message' => '网页授权获取用户基本信息错误，请检查appid等相关信息'
-                ];
-            }
-
-            $code = $_POST['code']; //用来换到accessToken的code
-            $api = new Api(Yii::$app->params['wx']['developer']['appId'], Yii::$app->params['wx']['developer']['appSecret']);
-
-            //获取AccessToken
-            $arr = $api->getOauthAccessToken($code);
-            var_dump($arr);die;
 
 
-            //获取用户信息
-            $userInfo = $api->getOauthUserInfo($arr['openid'], $arr['access_token']);
-            if($userInfo && MemberService::saveContact($userInfo)) {
-                Yii::warning(json_encode($userInfo));
-                return [
-                    'status' => 200,
-                    'message'=> 'success',
-                    'data'   => $userInfo
-                ];
-            }
-
+        if (!(isset($_POST['code']) || !isset($_POST['state']) )) {
+            Yii::error('网页授权获取用户基本信息错误，请检查appid等相关信息');
+            return [
+                'status' => 0,
+                'message' => '网页授权获取用户基本信息错误，请检查appid等相关信息'
+            ];
         }
+
+        $code = $_POST['code']; //用来换到accessToken的code
+        $api = new Api(Yii::$app->params['wx']['developer']['appId'], Yii::$app->params['wx']['developer']['appSecret']);
+
+        //获取AccessToken
+        $arr = $api->getOauthAccessToken($code);
+
+
+        //获取用户信息
+        $userInfo = $api->getOauthUserInfo($arr['openid'], $arr['access_token']);
+        if($userInfo && MemberService::saveContact($userInfo)) {
+            Yii::warning(json_encode($userInfo));
+            return [
+                'status' => 200,
+                'message'=> 'success',
+                'data'   => $userInfo
+            ];
+        }
+
         return [
             'status' => 0,
             'message' => '操作失败'
